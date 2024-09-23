e8071cd3fd3b51c6b0f9fdc8b226faacbb2de4b6
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2024-09-23 11:57:47 -0300

    Consumindo Api interna

--- aula13/src/app/produtos/page.tsx
+++ aula13/src/app/produtos/page.tsx
@@ -1,16 +1,24 @@
+"use client";
+import { TipoProduto } from "@/types";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
-    //Criar uma lista de objetos eletrônicos:
+    const [lista, setLista] = useState<TipoProduto[]>([])
 
-    const lista = [
-        {id:1,nome: 'Celular', preco: 1500, estoque: 10},
-        {id:2,nome: 'Tablet', preco: 800, estoque: 5},
-        {id:3,nome: 'Notebook', preco: 2500, estoque: 3},
-        {id:4,nome: 'Smartwatch', preco: 300, estoque: 20},
-        {id:5,nome: 'Fone de ouvido', preco: 200, estoque:15},
-    ];
+    useEffect(() => {
+
+        const chamadaApi = async ()=>{
+
+            const response = await fetch("http://localhost:3000/api");
+            const data = await response.json();
+            console.log(data);
+            setLista(data);
+        }
+        chamadaApi();
+
+    }, []);
 
   return (
     <div>
