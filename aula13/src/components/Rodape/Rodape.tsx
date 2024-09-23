38d476d6363e90038d15e58f4862fff102f17b7b
lecristina <cristinale33@hotmail.com>
2024-09-23 11:26:57 -0300

    api com router handlers com rotas estaticas

--- aula13/src/components/Rodape/Rodape.tsx
+++ aula13/src/components/Rodape/Rodape.tsx
@@ -3,7 +3,7 @@ export default function Rodape() {
   return (
     <footer>
         <h2>Rodapé</h2>
-        <p>Desenvolvido por <span>pf0670</span></p>
+        <p>Desenvolvido por <span>lecristina</span></p>
     </footer>
   )
 }
