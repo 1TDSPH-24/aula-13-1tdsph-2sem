cdf70e9aeda4e7c6e083bf1707bcc2f7d86dc238
ramonfn <felipenogueiraramon@gmail.com>
2024-09-23 11:57:33 -0300

    Consumo de API interna

--- aula13/src/app/produtos/page.tsx
+++ aula13/src/app/produtos/page.tsx
@@ -1,47 +1,55 @@
+"use client"
+import { TipoProduto } from "@/data/type";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
-    //Criar uma lista de objetos eletrônicos:
+    const [lista, setLista] = useState<TipoProduto[]>([]);
 
-    const lista = [
-        {id:1,nome: 'Celular', preco: 1500, estoque: 10},
-        {id:2,nome: 'Tablet', preco: 800, estoque: 5},
-        {id:3,nome: 'Notebook', preco: 2500, estoque: 3},
-        {id:4,nome: 'Smartwatch', preco: 300, estoque: 20},
-        {id:5,nome: 'Fone de ouvido', preco: 200, estoque:15},
-    ];
 
-  return (
-    <div>
-        <h1>Produtos</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nome</th>
-                    <th>Preço</th>
-                    <th>Estoque</th>
-                    <th>Editar</th>
-                </tr>
-            </thead>
-            <tbody>
-                {lista.map((produto) => (
-                    <tr key={produto.id}>
-                        <td>{produto.id}</td>
-                        <td>{produto.nome}</td>
-                        <td>{produto.preco}</td>
-                        <td>{produto.estoque}</td>
-                        <td> <Link href={`/produtos/produto/${produto.id}`}>Editar</Link> </td>
+    useEffect(() => {
+
+        const chamadaApi = async () => {
+
+            const response = await fetch("http://localhost:3000/api");
+            const data = await response.json();
+            console.log(data);
+            setLista(data);
+        }
+        chamadaApi();
+    }, []);
+
+    return (
+        <div>
+            <h1>Produtos</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th>Estoque</th>
+                        <th>Editar</th>
                     </tr>
+                </thead>
+                <tbody>
+                    {lista.map((produto) => (
+                        <tr key={produto.id}>
+                            <td>{produto.id}</td>
+                            <td>{produto.nome}</td>
+                            <td>{produto.preco}</td>
+                            <td>{produto.estoque}</td>
+                            <td> <Link href={`/produtos/produto/${produto.id}`}>Editar</Link> </td>
+                        </tr>
                     ))}
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colSpan={5}>Total de produtos: {lista.length}</td>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-  )
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={5}>Total de produtos: {lista.length}</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    )
 }
